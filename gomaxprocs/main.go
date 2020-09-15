4ec94f064813ca3505b02b66b579e7f47bb1fcb2
Mofizur Rahman <moficodes@gmail.com>
2020-09-14 22:51:53 -0400

    gomaxprocs example

--- gomaxprocs/main.go
+++ gomaxprocs/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
+func main() {
+	procs := runtime.GOMAXPROCS(2)
+	fmt.Println(procs)
+
+	var wg sync.WaitGroup
+	stop := make(chan int)
+	wg.Add(2)
+	go func() {
+		<-stop
+		defer wg.Done()
+		for number := 1; number < 1000; number++ {
+			fmt.Printf("A%d\t", number)
+		}
+	}()
+
+	go func() {
+		<-stop
+		defer wg.Done()
+		for number := 1; number < 1000; number++ {
+			fmt.Printf("B%d\t", number)
+		}
+	}()
+
+	close(stop)
+	wg.Wait()
 }

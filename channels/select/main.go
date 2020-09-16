3c295fac4043523230b5e89e7f0a360fa903b2c6
Mofizur Rahman <moficodes@gmail.com>
2020-09-15 22:07:11 -0400

    add select in loop

--- channels/select/main.go
+++ channels/select/main.go
@@ -19,5 +19,22 @@ func main() {
 		fmt.Println("path 1")
 	case c2 <- 1:
 		fmt.Println("path 2")
+		//default:
+		//	fmt.Println("nothing else was happening")
 	}
+
+	//count1, count2, count3 := 0, 0, 0
+	//close(c2)
+	//for i := 0; i < 10000; i++ {
+	//	select {
+	//	case <-c1:
+	//		count1++
+	//	case <-c2:
+	//		count2++
+	//	default:
+	//		count3++
+	//	}
+	//}
+	//
+	//fmt.Printf("count1: %d, count2: %d, count3: %d\n", count1, count2, count3)
 }
